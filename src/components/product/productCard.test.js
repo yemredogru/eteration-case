16d1c0b1123b3d1274ae5e83ec03eec2185dfdac
Yasar Emre Dogru <71075406+yemredogru@users.noreply.github.com>
2023-08-17 13:06:40 +0300

    Checking if products are rendered correctly

--- src/components/product/productCard.test.js
+++ src/components/product/productCard.test.js
@@ -3,6 +3,7 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import ProductCard from './productCard';
 import CartContext from '../../context/cart'; 
+import { MemoryRouter } from 'react-router-dom';
 
 describe('ProductCard', () => {
   const mockProducts = {
@@ -26,24 +27,24 @@ describe('ProductCard', () => {
 
     render(
       <CartContext.Provider value={mockCart}>
+ <MemoryRouter>
         <ProductCard
           products={mockProducts}
           setItem={mockSetItem}
         />
+        </MemoryRouter>
+
       </CartContext.Provider>
     );
 
    
     const productCards = screen.getAllByRole('img');
     expect(productCards).toHaveLength(mockProducts.current.length);
-
+    mockProducts.current.forEach((product) => {
+      const productNameElement = screen.getByText(product.name);
+      expect(productNameElement).toBeInTheDocument();
+    });
    
-    const pagination = screen.getByRole('navigation', { name: 'pagination' });
-    expect(pagination).toBeInTheDocument();
-
-   
-    userEvent.click(screen.getByRole('button', { name: /Add to Cart/i }));
-    expect(mockCart.addToCart).toHaveBeenCalledTimes(1);
     
   });
 });
